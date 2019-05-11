8fbad2e112c830dfd27b5386d6d9d5ec7bb857c4
agdavid <antonio.g.david@gmail.com>
2019-05-11 13:25:19 -0400

    declaring variables

--- projects/js-language-fundamentals/js-sandbox/app.js
+++ projects/js-language-fundamentals/js-sandbox/app.js
@@ -1,1 +1,54 @@
-alert('Hello Again')
+// console.log('Hello from the console');
+// console.log(123);
+
+// let variable = "Hello from variable";
+// console.log(variable);
+// let array = [4,5,6,7];
+// console.log(array);
+// let object = {a:1, b:2};
+// console.log(object);
+// console.table(object);
+// console.error("This is an error");
+
+// // var
+// var name = 'John Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+// // init var 
+// var greeting; // might do this in if conditional
+// console.log(greeting);
+// greeting = 'Hello';
+// console.log(greeting);
+
+// var camelCase = 'Camel Case';
+// console.log(camelCase);
+// var PascalCase = 'Pascal Case';
+// console.log(PascalCase);
+
+// let
+// let name = 'John Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+// const 
+// const name = 'John Doe';
+// console.log(name);
+// // cannot reassign
+// name = 'Steve Smith';
+// console.log(name);
+// // must assign value
+// const name;
+// can change data within value assigned to const, but cannot reassign value of const
+const person = {
+    name: 'John',
+    age: 30
+};
+console.log(person);
+person.name = 'Sara';
+console.log(person);
+const numbers = [1,2,3,4];
+numbers.push(5);
+console.log(numbers);
