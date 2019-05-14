2b32b8a92cc1914e6ceacd36265da42c0b82b61e
agdavid <antonio.g.david@gmail.com>
2019-05-13 22:43:56 -0400

    practice rtypes

--- projects/js-language-fundamentals/js-sandbox/app.js
+++ projects/js-language-fundamentals/js-sandbox/app.js
@@ -42,13 +42,43 @@
 // // must assign value
 // const name;
 // can change data within value assigned to const, but cannot reassign value of const
-const person = {
-    name: 'John',
-    age: 30
+// const person = {
+//     name: 'John',
+//     age: 30
+// };
+// console.log(person);
+// person.name = 'Sara';
+// console.log(person);
+// const numbers = [1,2,3,4];
+// numbers.push(5);
+// console.log(numbers);
+
+// PRIMITIVES
+// string
+const name = 'John Doe';
+console.log(typeof name);
+// number
+const age = 34;
+console.log(typeof age);
+// boolean
+const hasKids = false;
+console.log(typeof hasKids);
+// null
+const car = null;
+console.log(typeof null);
+// undefined
+let test;
+console.log(typeof test);
+// symbol
+const mySymbol = Symbol();
+console.log(typeof mySymbol);
+// REFERENCE TYPES - Objects
+const hobbies = ['music', 'movies'];
+console.log(typeof hobbies);
+const address = {
+    city: 'Rockville',
+    state: 'MD'
 };
-console.log(person);
-person.name = 'Sara';
-console.log(person);
-const numbers = [1,2,3,4];
-numbers.push(5);
-console.log(numbers);
+console.log(typeof address);
+const date = new Date();
+console.log(typeof date);
